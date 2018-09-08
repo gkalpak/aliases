1a2a98f6666e162ae7132b2056093e2a3007f2de
George Kalpakas <kalpakas.g@gmail.com>
2018-09-08 15:43:39 +0300

    fix(*): publish `bin` scripts to avoid relying on dependencies during installation

--- scripts/build.js
+++ scripts/build.js
@@ -2,9 +2,9 @@
 'use strict';
 
 // Imports
-const {run} = require('@gkalpak/cli-utils').commandUtils;
 const {mkdirSync, writeFileSync} = require('fs');
 const {join} = require('path');
+const {rm, set} = require('shelljs');
 const {ALIASES, BIN_DIR, DEF_CODE} = require('../lib/constants');
 const {getAliasCmd, getAliasSpec, onError} = require('../lib/utils');
 
@@ -13,26 +13,28 @@ _main();
 
 // Function - Definitions
 function _main() {
-  run(`rm -rf "${BIN_DIR}"`).
-    then(() => {
-      mkdirSync(BIN_DIR);
+  try {
+    set('-e');
+    rm('-rf', BIN_DIR);
+    mkdirSync(BIN_DIR);
 
-      Object.keys(ALIASES).forEach(categoryName => {
-        const category = ALIASES[categoryName];
-        const categoryDir = join(BIN_DIR, categoryName);
+    Object.keys(ALIASES).forEach(categoryName => {
+      const category = ALIASES[categoryName];
+      const categoryDir = join(BIN_DIR, categoryName);
 
-        mkdirSync(categoryDir);
+      mkdirSync(categoryDir);
 
-        Object.keys(category).forEach(aliasName => {
-          const file = join(categoryDir, `${aliasName}.js`);
-          const spec = getAliasSpec(category, aliasName);
-          const code = `${getAliasCode(spec)}\n`;
+      Object.keys(category).forEach(aliasName => {
+        const file = join(categoryDir, `${aliasName}.js`);
+        const spec = getAliasSpec(category, aliasName);
+        const code = `${getAliasCode(spec)}\n`;
 
-          writeFileSync(file, code);
-        });
+        writeFileSync(file, code);
       });
-    }).
-    catch(onError);
+    });
+  } catch (err) {
+    onError(err);
+  }
 }
 
 function getAliasCode(spec) {
