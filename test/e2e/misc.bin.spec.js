gkalpak/aliases

commit fac3a59510e2d023f33b733352803b618f7be6cd

deps(cli-utils): upgrade to v0.1.1
Also, update commands to use `::` for fallback sub-commands (which
should have been done already when upgrading to v0.1.0).

lib/constants.js
@@ -86,7 +86,7 @@ const ALIASES = {
     gs: 'git status $*',
     gl: 'git log --decorate $* || true',
     gl1: 'git log --decorate --oneline $* || true',
-    gsh: 'git show ${*:`__g-pick-commit --gkcu-returnOutput=1`}',
+    gsh: 'git show ${*:::__g-pick-commit --gkcu-returnOutput=1}',
     gd: 'ngm-diff-wh $*',
     gdh: 'ngm-diff-wh HEAD $*',
     gd1: 'ngm-diff-wh HEAD~1 $*',
@@ -98,7 +98,7 @@ const ALIASES = {
     gstp: 'git stash pop --index $*',
 
     // CHECKOUT
-    gco: 'git checkout ${*:`__g-pick-branch --gkcu-returnOutput=1`}',
+    gco: 'git checkout ${*:::__g-pick-branch --gkcu-returnOutput=1}',
     gcom: 'git checkout master $*',
     gcopr: 'git fetch upstream pull/$1/head && git checkout FETCH_HEAD',
 
@@ -108,8 +108,8 @@ const ALIASES = {
     gcmi: 'git commit $*',
     gcmf: 'git commit --all --fixup HEAD~${1:0} $2*',
     gcmif: 'git commit --fixup HEAD~${1:0} $2*',
-    gcmfc: 'git commit --all --fixup ${0:`__g-pick-commit --gkcu-returnOutput=1`} $*',
-    gcmifc: 'git commit --fixup ${0:`__g-pick-commit --gkcu-returnOutput=1`} $*',
+    gcmfc: 'git commit --all --fixup ${0:::__g-pick-commit --gkcu-returnOutput=1} $*',
+    gcmifc: 'git commit --fixup ${0:::__g-pick-commit --gkcu-returnOutput=1} $*',
     gcmfs: 'git commit --all --fixup :/$1 $2*',
     gcmifs: 'git commit --fixup :/$1 $2*',
     gcma: 'git commit --all --amend $*',
@@ -125,13 +125,13 @@ const ALIASES = {
     // BRANCH
     gb: 'git branch $*',
     gbc: 'git checkout master -b $1 && git push --verbose --set-upstream origin $1',
-    gbd: 'git branch --delete --force ${*:`__g-pick-branch --gkcu-returnOutput=1`}',
+    gbd: 'git branch --delete --force ${*:::__g-pick-branch --gkcu-returnOutput=1}',
 
     // PULL(-REBASE)
     gpr: 'git pull --rebase $*',
-    gpro: 'git pull --rebase origin ${*:`git rev-parse --abbrev-ref HEAD`}',
+    gpro: 'git pull --rebase origin ${*:::git rev-parse --abbrev-ref HEAD}',
     gprom: 'git pull --rebase origin master $*',
-    gpru: 'git pull --rebase upstream ${*:`git rev-parse --abbrev-ref HEAD`}',
+    gpru: 'git pull --rebase upstream ${*:::git rev-parse --abbrev-ref HEAD}',
     gprum: 'git pull --rebase upstream master $*',
 
     // SYNC MASTER
@@ -139,7 +139,7 @@ const ALIASES = {
 
 
     // REBASE
-    grb: 'git rebase ${*:`__g-pick-branch --gkcu-returnOutput=1`}',
+    grb: 'git rebase ${*:::__g-pick-branch --gkcu-returnOutput=1}',
     grbm: 'git rebase master $*',
     grbi: 'git rebase --interactive HEAD~$1',
     grbia: 'git rebase --autosquash --interactive HEAD~$1',
@@ -158,15 +158,15 @@ const ALIASES = {
     gcp: 'echo "  Cherry-picking SHA: $1  " && ' +
          'echo "----------------------------------------------------------------" && ' +
          'echo "                                                                " && ' +
-         'git checkout ${2:`__g-pick-branch --gkcu-returnOutput=1`} && ' +
-         'git pull --rebase origin ${2:`__g-pick-branch --gkcu-returnOutput=1`} && ' +
+         'git checkout ${2:::__g-pick-branch --gkcu-returnOutput=1} && ' +
+         'git pull --rebase origin ${2:::__g-pick-branch --gkcu-returnOutput=1} && ' +
          'git cherry-pick $1 && ' +
-         'ngm-diff-wh origin/${2:`__g-pick-branch --gkcu-returnOutput=1`} && ' +
+         'ngm-diff-wh origin/${2:::__g-pick-branch --gkcu-returnOutput=1} && ' +
          '(git log --decorate || true) && ' +
          'echo "                                                                " && ' +
          'echo "----------------------------------------------------------------" && ' +
          'echo "     >>>  (Don\'t forget to manually push the changes.)  <<<     "',
-    gcpl: 'gcp ${0:`git rev-parse HEAD`} ${1:`__g-pick-branch --gkcu-returnOutput=1`}',
+    gcpl: 'gcp ${0:::git rev-parse HEAD} ${1:::__g-pick-branch --gkcu-returnOutput=1}',
 
 
     // PRIVATE
@@ -246,9 +246,9 @@ const ALIASES = {
     // AIO-BUILDS DOCKER STUFF
     aiorm: 'docker stop aio & docker rm aio || true',
     aiobd: [
-      'yarn --cwd ${0:`__a-builds-dir`}/dockerbuild/scripts-js --frozen-lockfile --non-interactive install',
-      'yarn --cwd ${0:`__a-builds-dir`}/dockerbuild/scripts-js build',
-      'docker build --tag aio-builds $* ${0:`__a-builds-dir`}/dockerbuild',
+      'yarn --cwd ${0:::__a-builds-dir}/dockerbuild/scripts-js --frozen-lockfile --non-interactive install',
+      'yarn --cwd ${0:::__a-builds-dir}/dockerbuild/scripts-js build',
+      'docker build --tag aio-builds $* ${0:::__a-builds-dir}/dockerbuild',
     ].join(' && '),
     aiord: 'docker run -d --dns 127.0.0.1 --name aio -p 80:80 -p 443:443 $* aio-builds',
     aioatt: 'docker exec -it aio /bin/bash',

commit 316c4053ff11562886b96548ad33428b84145272

fix(*): correctly replace private aliases in descriptions
This was accidentally broken in fac3a5951 by upgrading to
@gkalpak/cli-utils v0.1.1.

lib/alias.js
@@ -49,7 +49,7 @@ class AliasDefault extends Alias {
 }
 
 class AliasUnknown extends Alias {
-  static get DESCRIPTION() { return '__unknown-alias'; }
+  static get DESCRIPTION() { return '__unknownAlias__'; }
 
   constructor() {
     super(new AliasSpec('', AliasUnknown.DESCRIPTION));

lib/constants.js
@@ -14,9 +14,9 @@ const SOURCE_NVM_CMD = '. $NVM_DIR/nvm.sh';
 
 const DESC_REPLACEMENTS = {
   [AliasUnknown.DESCRIPTION]: '???',
-  '`__a-builds-dir`': '(<.../angular/aio/aio-builds-setup>)',
-  '`__g-pick-branch --gkcu-returnOutput=1`': '(interactively pick a branch)',
-  '`__g-pick-commit --gkcu-returnOutput=1`': '(interactively pick a commit)',
+  '::__a-builds-dir': '(<.../angular/aio/aio-builds-setup>)',
+  '::__g-pick-branch --gkcu-returnOutput=1': '(interactively pick a branch)',
+  '::__g-pick-commit --gkcu-returnOutput=1': '(interactively pick a commit)',
 };
 
 const DEF_CODE = AliasSpecDefault.DEF_CODE = (cmd, cfg = {}) => utils.stripIndentation(`

test/e2e/misc.bin.spec.js
@@ -145,6 +145,13 @@ describe(SCRIPT_DIR, testingUtils.withJasmineTimeout(30000, () => {
       expect(result).not.toContain('Unknown aliases');
       expect(result).not.toContain('xyz');
     });
+
+    it('should replace private aliases in descriptions', async () => {
+      const result = await testScript();
+      expect(result).not.toContain('__');
+      expect(result).toContain('(interactively pick a branch)');
+      expect(result).toContain('(interactively pick a commit)');
+    });
   });
 
   describe('ll', () => {