5636e3dd1ebbaa86d20659bb1d1cf0a922423976
George Kalpakas <kalpakas.g@gmail.com>
2022-09-01 11:39:49 +0300

    test(git): fix failing test on CI runs for tags
    When tests are run on CI for a tag, then the branch refs are not
    available (due to how checking out works). This lead to the `gdefb` e2e
    tests failing.
    
    Account for this to make tests pass.

--- test/e2e/git.bin.spec.js
+++ test/e2e/git.bin.spec.js
@@ -12,11 +12,20 @@ const SCRIPT_DIR = 'bin/git/';
 describe(SCRIPT_DIR, () => {
   describe('gdefb', () => {
     const testScript = testingUtils.testScriptFactory(join(ROOT_DIR, SCRIPT_DIR, 'gdefb'));
+    let isOnCiTag;
+
+    beforeAll(async () => {
+      // If this test run is on CI for a tag, then the branch refs may not be available.
+      // NOTE: This may also affect non-master/main branches, but these are currently not used.
+      isOnCiTag = Boolean(process.env.CI) &&
+        /^v\d+\.\d+\.\d+$/.test(await testingUtils.testCmd('git describe --tags || true')) &&
+        /^$/.test(await testingUtils.testCmd('git show-ref --heads || true'));
+    });
 
     it('should detect and print the default branch', async () => {
       const result = await testScript();
 
-      expect(result).toBe('master');
+      expect(result).toBe(isOnCiTag ? 'unknown-branch' : 'master');
     });
   });
 });
