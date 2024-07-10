bcfaeea8793f3580e6568c9b8c379a86699d9b9d
George Kalpakas <kalpakas.g@gmail.com>
2024-07-10 13:26:36 +0300

    feat(git): add support for filtering commits in `__p-pick-commit` alias
    Add support for filtering the list of commits to choose from in the
    `__g-pick-commits` alias by passing a `--filter-commits` argument. The
    value of the argument will be used as a `grep` Basic Regular Expression
    pattern to filter the commits (based on their headers; i.e. first line).

--- test/unit/alias-scripts/g-pick-commit.spec.js
+++ test/unit/alias-scripts/g-pick-commit.spec.js
@@ -23,55 +23,123 @@ describe('g-pick-commit', () => {
     });
 
     it('should delegate to its internal counterpart', async () => {
-      const mockConfig = {foo: 'bar'};
-      const internalSpy = spyOn(_testing, '_gPickCommit').and.resolveTo('foo');
+      const mockArgs = ['--foo', 'bar'];
+      const mockConfig = {baz: 'qux'};
+      const internalSpy = spyOn(_testing, '_gPickCommit').and.resolveTo('quux');
 
-      expect(await gPickCommit(mockConfig)).toBe('foo');
-      expect(internalSpy).toHaveBeenCalledWith(mockConfig);
+      expect(await gPickCommit(mockArgs, mockConfig)).toBe('quux');
+      expect(internalSpy).toHaveBeenCalledWith(mockArgs, mockConfig);
     });
 
     describe('(dryrun)', () => {
       it('should return a resolved promise', async () => {
-        const promise = gPickCommit({dryrun: true});
+        const promise = gPickCommit([], {dryrun: true});
         expect(promise).toEqual(jasmine.any(Promise));
 
         await promise;
       });
 
-      it('should log a short description', async () => {
-        const cmdDesc = 'Pick one from a list of commits.';
-        await gPickCommit({dryrun: true});
+      it('should log a short description (when not filtering commits)', async () => {
+        const cmdDesc = 'Pick one from the first 50 commits.';
+        await gPickCommit([], {dryrun: true});
 
         expect(consoleLogSpy).toHaveBeenCalledWith(cmdDesc);
+      });
+
+      it('should log a short description (when filtering commits)', async () => {
+        const cmdDesc = 'Pick one from the first 50 commits whose header matches \'foo or bar\'.';
+        const runtimeArgsList = [
+          ['--filter-commits', 'foo or bar'],
+          ['--filter-commits', '"foo or bar"'],
+          ['--filter-commits', '\'foo or bar\''],
+          ['--filter-commits=foo or bar'],
+          ['--filter-commits="foo or bar"'],
+          ['--filter-commits=\'foo or bar\''],
+        ];
+
+        for (const testArgs of runtimeArgsList) {
+          consoleLogSpy.calls.reset();
+          await gPickCommit(testArgs, {dryrun: true});
+
+          expect(consoleLogSpy).withContext(`With args: ${testArgs.join(', ')}`).toHaveBeenCalledWith(cmdDesc);
+        }
       });
     });
 
     describe('(no dryrun)', () => {
       it('should return a promise', async () => {
-        const promise = gPickCommit({});
-        expect(promise).toEqual(jasmine.any(Promise));
+        const promise1 = gPickCommit();
+        expect(promise1).toEqual(jasmine.any(Promise));
 
-        await promise;
+        const promise2 = gPickCommit([]);
+        expect(promise2).toEqual(jasmine.any(Promise));
+
+        const promise3 = gPickCommit([], {});
+        expect(promise3).toEqual(jasmine.any(Promise));
+
+        await Promise.all([promise1, promise2, promise3]);
       });
 
       it('should run `git log ...` (and return the output)', async () => {
-        await gPickCommit({});
-        expect(cmdUtilsRunSpy).toHaveBeenCalledWith('git log --oneline -50', [], {returnOutput: true});
+        await gPickCommit();
+        expect(cmdUtilsRunSpy).toHaveBeenCalledWith('git log --oneline --max-count=50', [], {returnOutput: true});
       });
 
       it('should return `git log ...` output even if `config.returnOutput` is false (but not affect `config`)',
           async () => {
             const config = {returnOutput: false};
-            await gPickCommit(config);
+            await gPickCommit([], config);
 
-            expect(cmdUtilsRunSpy).toHaveBeenCalledWith('git log --oneline -50', [], {returnOutput: true});
+            expect(cmdUtilsRunSpy).toHaveBeenCalledWith('git log --oneline --max-count=50', [], {returnOutput: true});
             expect(config.returnOutput).toBe(false);
           }
       );
 
       it('should propagate errors', async () => {
         cmdUtilsRunSpy.and.rejectWith('test');
-        await expectAsync(gPickCommit({})).toBeRejectedWith('test');
+        await expectAsync(gPickCommit()).toBeRejectedWith('test');
+      });
+
+      describe('filtering commits', () => {
+        let runtimeArgsList;
+
+        beforeEach(() => {
+          runtimeArgsList = [
+            ['--filter-commits', 'foo or bar'],
+            ['--filter-commits', '"foo or bar"'],
+            ['--filter-commits', '\'foo or bar\''],
+            ['--filter-commits=foo or bar'],
+            ['--filter-commits="foo or bar"'],
+            ['--filter-commits=\'foo or bar\''],
+          ];
+        });
+
+        it('should run `git log ... | grep ...` (and return the output)', async () => {
+          for (const testArgs of runtimeArgsList) {
+            cmdUtilsRunSpy.calls.reset();
+            await gPickCommit(testArgs);
+
+            expect(cmdUtilsRunSpy).withContext(`With args: ${testArgs.join(', ')}`).toHaveBeenCalledWith(
+                'git log --oneline | grep "foo or bar" --max-count=50', [], {returnOutput: true});
+          }
+        });
+
+        it(
+            'should return `git log ... | grep ...` output even if `config.returnOutput` is false (but not affect ' +
+            '`config`)',
+            async () => {
+              for (const testArgs of runtimeArgsList) {
+                cmdUtilsRunSpy.calls.reset();
+
+                const config = {returnOutput: false};
+                await gPickCommit(testArgs, config);
+
+                expect(cmdUtilsRunSpy).withContext(`With args: ${testArgs.join(', ')}`).toHaveBeenCalledWith(
+                    'git log --oneline | grep "foo or bar" --max-count=50', [], {returnOutput: true});
+                expect(config.returnOutput).toBe(false);
+              }
+            }
+        );
       });
 
       describe('picking a commit', () => {
@@ -91,7 +159,7 @@ describe('g-pick-commit', () => {
         });
 
         it('should prompt the user to pick a commit', async () => {
-          await gPickCommit({});
+          await gPickCommit();
 
           verifyPromptedWith('type', 'list');
           verifyPromptedWith('message', 'Pick a commit:');
@@ -104,7 +172,7 @@ describe('g-pick-commit', () => {
             '3456789 The baz commit',
             '456789 The qux commit',
           ];
-          await gPickCommit({});
+          await gPickCommit();
 
           verifyPromptedWith('choices', [
             '123456 The foo commit',
@@ -121,7 +189,7 @@ describe('g-pick-commit', () => {
             '\t\t3456789 The baz commit\t\t',
             ' \n 456789 The qux commit \t ',
           ];
-          await gPickCommit({});
+          await gPickCommit();
 
           verifyPromptedWith('choices', [
             '123456 The foo commit',
@@ -140,7 +208,7 @@ describe('g-pick-commit', () => {
             ' \t\r\n ',
             '456789 The qux commit',
           ];
-          await gPickCommit({});
+          await gPickCommit();
 
           verifyPromptedWith('choices', [
             '123456 The foo commit',
@@ -157,7 +225,7 @@ describe('g-pick-commit', () => {
             '3456789 The baz commit',
             '456789 The qux commit',
           ];
-          await gPickCommit({});
+          await gPickCommit();
 
           verifyPromptedWith('default', 0);
         });
@@ -171,7 +239,7 @@ describe('g-pick-commit', () => {
             return Promise.resolve({commit: ''});
           });
 
-          await gPickCommit({});
+          await gPickCommit();
 
           const processExitSpy = spyOn(process, 'exit');
 
@@ -195,7 +263,7 @@ describe('g-pick-commit', () => {
             return Promise.resolve({commit: ''});
           });
 
-          await gPickCommit({});
+          await gPickCommit();
 
           expect(promptSpy).toHaveBeenCalledTimes(1);
           expect(unlistenSpy).toHaveBeenCalledWith();
@@ -211,7 +279,7 @@ describe('g-pick-commit', () => {
             return Promise.reject('');
           });
 
-          await expectAsync(gPickCommit({})).toBeRejected();
+          await expectAsync(gPickCommit()).toBeRejected();
 
           expect(promptSpy).toHaveBeenCalledTimes(1);
           expect(unlistenSpy).toHaveBeenCalledWith();
@@ -221,21 +289,29 @@ describe('g-pick-commit', () => {
       describe('output', () => {
         it('should log the selected commit SHA (removing other info)', async () => {
           promptSpy.and.returnValues(
-              Promise.resolve({commit: 'f00ba2 (foo, origin/baz) This is the foo commit message'}),
-              Promise.resolve({commit: 'b4r9ux (bar, origin/qux) This is the bar commit message'}));
+              Promise.resolve({commit: 'f00f00 (foo, origin/foo) This is the foo commit message'}),
+              Promise.resolve({commit: 'b4rb4r (bar, origin/bar) This is the bar commit message'}),
+              Promise.resolve({commit: 'b4zb4z (baz, origin/baz) This is the baz commit message'}),
+              Promise.resolve({commit: '9ux9ux (qux, origin/qux) This is the qux commit message'}));
 
-          expect(await gPickCommit({})).toBeUndefined();
-          expect(consoleLogSpy).toHaveBeenCalledWith('f00ba2');
+          expect(await gPickCommit()).toBeUndefined();
+          expect(consoleLogSpy).toHaveBeenCalledWith('f00f00');
 
-          expect(await gPickCommit({returnOutput: false})).toBeUndefined();
-          expect(consoleLogSpy).toHaveBeenCalledWith('b4r9ux');
+          expect(await gPickCommit([])).toBeUndefined();
+          expect(consoleLogSpy).toHaveBeenCalledWith('b4rb4r');
+
+          expect(await gPickCommit([], {})).toBeUndefined();
+          expect(consoleLogSpy).toHaveBeenCalledWith('b4zb4z');
+
+          expect(await gPickCommit([], {returnOutput: false})).toBeUndefined();
+          expect(consoleLogSpy).toHaveBeenCalledWith('9ux9ux');
         });
 
         it('should return the selected commit SHA (removing other info) if `returnOutput` is `true`', async () => {
-          const commit = 'f00ba2 (foo, origin/baz) This is the commit message';
+          const commit = 'f00b4r (baz, origin/qux) This is the commit message';
           promptSpy.and.resolveTo({commit});
 
-          expect(await gPickCommit({returnOutput: true})).toBe('f00ba2');
+          expect(await gPickCommit([], {returnOutput: true})).toBe('f00b4r');
           expect(consoleLogSpy).not.toHaveBeenCalled();
         });
       });
@@ -251,11 +327,11 @@ describe('g-pick-commit', () => {
       expect(main).toEqual(jasmine.any(Function));
     });
 
-    it('should delegate to `gPickCommit()` (with appropriate arguments)', () => {
-      gPickCommitSpy.and.returnValue('foo');
-      const result = main('runtimeArgs', 'config');
+    it('should delegate to `gPickCommit()` (with appropriate arguments)', async () => {
+      gPickCommitSpy.and.resolveTo('foo');
+      const result = await main('runtimeArgs', 'config');
 
-      expect(gPickCommitSpy).toHaveBeenCalledWith('config');
+      expect(gPickCommitSpy).toHaveBeenCalledWith('runtimeArgs', 'config');
       expect(result).toBe('foo');
     });
   });
